fbf0eb3aa4377ee3c0f8b17893f2512ff0257d43
lordozb <aadityaja@gmail.com>
2017-05-16 17:05:45 +0530

    A wonderful program!

--- Desktop/Demo.java
+++ Desktop/Demo.java
@@ -1,212 +1,54 @@
 import java.util.*;
 
-class Node{
-  int val;
-  Node left;
-  Node right;
+/*
+ * WORDS ARE SORTED FREQUENCY WISE.
+ * IF FREQUENCY IS SAME THEN WORDS ARE SORTED ALPHABETICALLY.
+ */
 
-  public Node(int val){
-    this.val = val;
-    this.left = null;
-    this.right = null;
-  }
-}
-class BST{
-  Node root;
-  Node crawler;
-  public BST(){
-    Node root = null;
-  }
+class Foo{
+  String name;
+  int freq;
 
-  public void add(int x){
-    if(root == null){
-      root = new Node(x);
-      crawler = root;
-    }
-    else{
-      if(x < crawler.val){
-        if(crawler.left == null){
-          crawler.left = new Node(x);
-          return;
-        }
-        else{
-          crawler = crawler.left;
-          add(x);
-        }
-
-      }
-      else{
-        if(crawler.right == null){
-          crawler.right = new Node(x);
-          return;
-        }
-        else{
-          crawler = crawler.right;
-          add(x);
-        }
-      }
-    }
-    crawler = root;
-  }
-
-  public void inorder(){
-    printInorder(root);
-    System.out.println("");
-  }
-
-  public void printInorder(Node root){
-    if(root == null)
-      return;
-    printInorder(root.left);
-    System.out.println(root.val);
-    printInorder(root.right);
-  }
-
-  public void preorder(){
-    printpreorder(root);
-    System.out.println("");
-  }
-
-  public void printpreorder(Node root){
-    if(root == null)
-      return;
-    System.out.println(root.val);
-    printpreorder(root.left);
-    printpreorder(root.right);
-
-  }
-  public void postorder(){
-    printpostorder(root);
-    System.out.println("");
-  }
-
-  public void printpostorder(Node root){
-    if(root == null)
-      return;
-    printpostorder(root.left);
-    printpostorder(root.right);
-    System.out.println(root.val);
-
-  }
-
-  public void min(){
-    crawler = root;
-    if(crawler == null)
-      return;
-    else{
-      while(crawler.left != null)
-        crawler = crawler.left;
-      System.out.println(crawler.val);
-    }
-    crawler = root;
-  }
-
-  public void max(){
-    crawler = root;
-    if(crawler == null)
-      return;
-    else{
-      while(crawler.right != null)
-        crawler = crawler.right;
-      System.out.println(crawler.val);
-    }
-    crawler = root;
-  }
-  public void search(int x){
-
-    if(crawler == null){
-      System.out.println("NO");
-      return;
-    }
-    if(x < crawler.val){
-      crawler = crawler.left;
-      search(x);
-    }
-
-    else if(x > crawler.val){
-      crawler = crawler.right;
-      search(x);
-    }
-    else{
-      System.out.println("found");
-    }
-      return;
-  }
-
-  public void height(){
-    crawler = root;
-    int x = findHeight(crawler);
-    System.out.println(x);
-    crawler = root;
-  }
-  public int findHeight(Node crawler){
-    if(crawler == null)
-      return -1;
-    return(1+ Math.max(findHeight(crawler.left), findHeight(crawler.right)));
-  }
-
-  public void delete(int x){
-    crawler = root;
-    deleteElement(crawler, x);
-  }
-
-  public void deleteElement(Node root, int x){
-    Node node = searchElement(root, x);
-    if(node == null){
-      System.out.println("ELEMENT not found");
-      return;
-    }
-    if(node.left == null && node.right == null)
-      node = null;
-    else if(node.left == null){
-      node = node.right;
-    }
-
-    else if(node.right == null){
-      node = node.left;
-    }
-    else{
-      int y = searchMax(node.left);
-      node.val = y;
-      deleteElement(node.left, y);
-
-    }
-  }
-  public Node searchElement(Node root, int x){
-    if(root == null)
-      return null;
-    if(x < root.val)
-      searchElement(root.left, x);
-    else if(x > root.val)
-      searchElement(root.right, x);
-
-      return root;
-
-  }
-
-  public int searchMax(Node root){
-    while(root.right != null)
-      root = root.right;
-    return root.val;
+  public Foo(String name){
+    this.name = name;
+    this.freq = 1;
   }
 }
 public class Demo{
   public static void main(String[] args) {
-    BST bst = new BST();
-    bst.add(3);
-    bst.add(1);
-    bst.add(5);
-    bst.add(0);
-    bst.add(2);
-    bst.inorder();
-  /*  bst.preorder();
-    bst.postorder();
-    bst.max();
-    bst.min();
-    bst.search(2);
-    bst.search(10);
-    bst.height(); */
-    bst.delete(10);
-    bst.inorder();
+      ArrayList <Foo> al = new ArrayList < >();
+      int index = 0;
+      HashMap <String, Integer> hm = new HashMap < > ();
+      String[] names = {"ac", "ba", "c", "ab", "aa", "ab", "bb", "bb","ac"};
+      for(int i = 0; i < names.length; i++){
+        String str = names[i];
+        if(hm.containsKey(str)){
+          int ind = hm.get(str);
+          al.get(ind).freq += 1;
+        }
+        else{
+          al.add(new Foo(str));
+          hm.put(str, index);
+          index++;
+        }
+      }
+
+      Collections.sort(al, new Comparator <Foo>(){
+        public int compare(Foo o1, Foo o2){
+          if(o1.freq < o2.freq)
+            return -1;
+          else if (o1.freq > o2.freq)
+            return 1;
+          else{
+            if(o1.name.compareTo(o2.name) < 0)
+              return -1;
+            else
+              return 1;
+          }
+        }
+      });
+
+      for(Foo x : al)
+        System.out.println(x.name+" "+x.freq);
   }
 }
